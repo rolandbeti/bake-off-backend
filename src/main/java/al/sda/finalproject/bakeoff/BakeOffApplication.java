a5008f1f6a3847d0ed4bd879b110e8908ff5159e
Roland Beti <rolandbeti@gmail.com>
2022-07-03 11:18:23 +0200

    Initial commit

---
+++ src/main/java/al/sda/finalproject/bakeoff/BakeOffApplication.java
@@ -0,0 +1,13 @@
+package al.sda.finalproject.bakeoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeOffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/sda/finalproject/bakeoff/BakeOffApplicationTests.java
@@ -0,0 +1,13 @@
+package al.sda.finalproject.bakeoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

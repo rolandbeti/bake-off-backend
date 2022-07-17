546071f1b27d8f72ed7fafa625bfd9a92c794eb7
Roland Beti <rolandbeti@gmail.com>
2022-07-18 00:32:07 +0200

    BE-212 Generate Token from Jwts Builder

---
+++ src/main/java/al/sda/finalproject/bakeoff/security/JwtUtils.java
@@ -0,0 +1,50 @@
+package al.sda.finalproject.bakeoff.security;
+
+import al.sda.finalproject.bakeoff.security.impl.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private String jwtSecret = "klasjdajsldkhalblabsflbalsda";
+    private int jwtExpirationMs = 20000000;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String generateJwtToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+    public String getUserNameFromJwtToken(String token) {
+
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+    public boolean validateJwtToken(String authToken) {
+        String jwtSecret = "klasjdajsldkhalblabsflbalsda";
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+        } catch (ExpiredJwtException e) {
+        } catch (UnsupportedJwtException e) {
+        } catch (IllegalArgumentException e) {
+        }
+        return false;
+    }
+}
